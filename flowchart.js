847ffcd6e9303ccdb905e232417dde4e0a09b876
mmazo <michail.mazo@gmail.com>
2015-09-03 21:10:06 +0200

    moved from dom-based data and added exportChart.

--- flowchart.js
+++ flowchart.js
@@ -40,8 +40,45 @@ FlowChart.prototype.nextElementIndex = function(){
 
 FlowChart.prototype.connections = {};
 
+FlowChart.prototype.addOrUpdateConnectionInConnectionsObject = function(connId, startNodeId, endNodeId, connD){
+	this.connections[connId] = {
+		start: startNodeId,
+		end: endNodeId,
+		d: connD
+	};
+};
+
+FlowChart.prototype.deleteConnectionFromConnectionsObject = function(connId){
+	delete this.connections[connId];
+};
+
+FlowChart.prototype.getConnectionFromConnectionsObject = function(connId){
+	return this.connections[connId];
+};
+
+FlowChart.prototype.nodes = {};
+
+FlowChart.prototype.addOrUpdateNodeInNodesObject = function(nodeId, nodeType, nodeContent, nodeLeftPos, nodeTopPos, startpointFor, endpointFor){
+	this.nodes[nodeId] = {
+		type: nodeType, 
+		content: nodeContent, 
+		left: nodeLeftPos, 
+		top: nodeTopPos, 
+		startFor: startpointFor, 
+		endFor: endpointFor
+	};
+};
+
+FlowChart.prototype.deleteNodeFromNodesObject = function(nodeId){
+	delete this.nodes[nodeId];
+};
+
+FlowChart.prototype.getNodeFromNodesObject = function(nodeId){
+	return this.nodes[nodeId];
+};
+
 FlowChart.prototype.positionConnectionPoint = function(left, top, linePointType, lineObj, finishArrangement){
-    var d = this.connections[lineObj.attr('id')].split(' ');
+    var d = this.getConnectionFromConnectionsObject(lineObj.attr('id')).d.split(' ');
     if (linePointType === 'endpoint') {
       d[3] = left + ',' + top;
       if (finishArrangement) {
@@ -56,7 +93,7 @@ FlowChart.prototype.positionConnectionPoint = function(left, top, linePointType,
     }
 	d = d.join(' ');
     lineObj.attr('d', d);
-	this.connections[lineObj.attr('id')] = d;
+	this.connections[lineObj.attr('id')].d = d;
 }
   
 FlowChart.prototype.positionConnectionLines = function(nodeObj, finishArrangement){
@@ -65,23 +102,19 @@ FlowChart.prototype.positionConnectionLines = function(nodeObj, finishArrangemen
      var left = t.offset().left + t.outerWidth() / 2;
      var top = t.offset().top + t.outerHeight() / 2;
       
-     var linesRight = t.attr('startpoint-for');
-     var linesLeft = t.attr('endpoint-for');
+     var linesRight = _t.nodes[t.attr('id')].startFor;
+     var linesLeft = _t.nodes[t.attr('id')].endFor;
       
-     if (linesRight) {
-        linesRight = linesRight.split(',');
         for (var i = 0; i < linesRight.length; i++)
         {
           this.positionConnectionPoint(left, top, 'startpoint', _t.jQueryLibraryObj('#' + linesRight[i]), finishArrangement);
         }
-     }
-     if (linesLeft) {
-        linesLeft = linesLeft.split(',');
+     
+
         for (var i = 0; i < linesLeft.length; i++)
         {
           this.positionConnectionPoint(left, top, 'endpoint', _t.jQueryLibraryObj('#' + linesLeft[i]), finishArrangement);
         }
-     }
 }
 
 FlowChart.prototype.addNode = function(nodeType, parentNode) {
@@ -89,19 +122,25 @@ FlowChart.prototype.addNode = function(nodeType, parentNode) {
   
   // add new node to nodes layer
   var nodesLayer = this.jQueryLibraryObj('#nodes-layer');
-  var currentDate = new Date();
   var nodeId = "node-" + _t.nextElementIndex();
   var nodeClass = 'node ' + nodeType;
   var node = "<div id='" + nodeId + "' class='" + nodeClass + "'>" + this.nodeTemplates[nodeType] + "</div>";
   nodesLayer.append(node);
   node = this.jQueryLibraryObj('#' + nodeId);
   
+  _t.addOrUpdateNodeInNodesObject(nodeId, nodeType, _t.nodeTemplates[nodeType], 0, 0, [], []);
+  
   if (!parentNode){
     // position the created node element
     node.offset(_t.rootElementPosition);
+	_t.nodes[nodeId].left = _t.rootElementPosition.left;
+	_t.nodes[nodeId].top = _t.rootElementPosition.top;
   }else{
     // position the created node element
     node.offset({left: parentNode.offset().left + _t.offsetForNextElement, top: parentNode.offset().top});
+	
+	_t.nodes[nodeId].left = parentNode.offset().left + _t.offsetForNextElement;
+	_t.nodes[nodeId].top = parentNode.offset().top;
     
     // add connection line and register it to start and end nodes
     _t.addConnectionBetween(parentNode, node);
@@ -114,6 +153,8 @@ FlowChart.prototype.addNode = function(nodeType, parentNode) {
     },
     stop: function() {
       var t = _t.jQueryLibraryObj(this);
+	  _t.nodes[t.attr('id')].left = t.offset().left;
+	  _t.nodes[t.attr('id')].top = t.offset().top;
       _t.positionConnectionLines(t, true);
       _t.adjustLayersSize();
       if (_t.nodeActionEndEffect) {
@@ -141,48 +182,46 @@ FlowChart.prototype.addNode = function(nodeType, parentNode) {
   if (_t.nodeActionEndEffect) {
       node.effect(_t.nodeActionEndEffect, {}, 500, function(){});
   }
-  
+   
   return node;
 }
 
 FlowChart.prototype.deleteNode = function(nodeId){
   var _t = this;
   var node = _t.jQueryLibraryObj('#' + nodeId);
-  var linesRight = node.attr('startpoint-for');
-  var linesLeft = node.attr('endpoint-for');   
+  var nodeObj = _t.getNodeFromNodesObject(nodeId);
+  var linesRight = nodeObj.startFor;
+  var linesLeft = nodeObj.endFor;   
   node.remove();
-  if (linesRight) {
-    linesRight = linesRight.split(',');
+  _t.deleteNodeFromNodesObject(nodeId);
+  
     for (var i = 0; i < linesRight.length; i++) {
       _t.deleteConnection(linesRight[i]);
     }
-  }
-  if (linesLeft) {
-    linesLeft = linesLeft.split(',');
+
     for (var i = 0; i < linesLeft.length; i++) {
       _t.deleteConnection(linesLeft[i]);
     }
-  }
 }
 
 FlowChart.prototype.deleteConnection = function(connectionId){
   var conn = this.jQueryLibraryObj('#' + connectionId);
-  var startNode = this.jQueryLibraryObj('#' + conn.attr('data-startnode'));
-  var endNode = this.jQueryLibraryObj('#' + conn.attr('data-endnode'));
-  if(startNode.length === 1){
-    var linesRight = startNode.attr('startpoint-for').split(',');
+  var connObj = this.getConnectionFromConnectionsObject(connectionId);
+  var startNode = this.nodes[connObj.start];
+  var endNode = this.nodes[connObj.end];
+  if(startNode){
+    var linesRight = startNode.startFor;
     var index = linesRight.indexOf(connectionId);
     linesRight.splice(index,1);
-    linesRight = linesRight.join(',');
-    startNode.attr('startpoint-for', linesRight);
+    this.nodes[connObj.start].startFor = linesRight;
   }
-  if(endNode.length === 1){
-    var linesLeft = endNode.attr('endpoint-for').split(',');
+  if(endNode){
+    var linesLeft = endNode.endFor;
     var index = linesLeft.indexOf(connectionId);
     linesLeft.splice(index,1);
-    linesLeft = linesLeft.join(',');
-    endNode.attr('endpoint-for', linesLeft);
+    this.nodes[connObj.end].endFor = linesLeft;
   }
+  this.deleteConnectionFromConnectionsObject(connectionId);
   conn.remove();
 }
 
@@ -192,25 +231,13 @@ FlowChart.prototype.addConnectionBetween = function(startNode, endNode){
     // add connection line and register it to start and end nodes
     var conId = this.addConnection(startNode, endNode);
 
-    var startpointFor = startNode.attr('startpoint-for');
-    if (!startpointFor) {
-        startpointFor = [];
-    } else {
-        startpointFor = startpointFor.split(',');
-    }
-    startpointFor.push(conId)
-    startpointFor = startpointFor.join(',');
-    startNode.attr('startpoint-for', startpointFor);
+    var startpointFor = this.nodes[startNode.attr('id')].startFor;
+    startpointFor.push(conId);
+    this.nodes[startNode.attr('id')].startFor = startpointFor;
 
-    var endpointFor = endNode.attr('endpoint-for');
-    if (!endpointFor) {
-        endpointFor = [];
-    } else {
-        endpointFor = endpointFor.split(',');
-    }
+    var endpointFor = this.nodes[endNode.attr('id')].endFor;
     endpointFor.push(conId);
-    endpointFor.join(',');
-    endNode.attr('endpoint-for', endpointFor);
+    this.nodes[endNode.attr('id')].endFor = endpointFor;
 
     // reposition connection lines
     _t.positionConnectionLines(startNode, true);
@@ -226,15 +253,14 @@ FlowChart.prototype.addConnection = function(startNode, endNode) {
                        " C" + startNodePos.left + "," + startNodePos.top + 
                        " " + endNodePos.left + "," + endNodePos.top + 
                        " " + endNodePos.left + "," + endNodePos.top;
+  
   var newpath = document.createElementNS("http://www.w3.org/2000/svg","path");
-  newpath.setAttributeNS(null, "id", "line-" + this.nextElementIndex() );  
-  newpath.setAttributeNS(null, "d", conCoordinates);
-  newpath.setAttributeNS(null, "data-startnode", startNode.attr('id'));
-  newpath.setAttributeNS(null, "data-endnode", endNode.attr('id'));  
+      newpath.setAttributeNS(null, "id", "line-" + this.nextElementIndex() );  
+      newpath.setAttributeNS(null, "d", conCoordinates);
   
   conLayer[0].appendChild(newpath);
-	 	 	 			 			 							 
-	this.connections["line-" + this.elementIndex] = conCoordinates;
+	 	 	 			 			 							 	
+  this.addOrUpdateConnectionInConnectionsObject("line-" + this.elementIndex, startNode.attr('id'), endNode.attr('id'), conCoordinates);
    
   return "line-" + this.elementIndex;
 }
@@ -242,4 +268,33 @@ FlowChart.prototype.addConnection = function(startNode, endNode) {
 FlowChart.prototype.adjustLayersSize = function() {
   this.jQueryLibraryObj('.layer').width(this.jQueryLibraryObj(document).width())
                                  .height(this.jQueryLibraryObj(document).height());
+}
+
+FlowChart.prototype.exportChart = function(){
+	var nodes = this.nodes;
+	var conns = this.connections;
+	var chartJSON = {
+		nodes: [],
+		connections: []
+	};
+	
+	for (var node in nodes) {
+     if (nodes.hasOwnProperty(node)) {
+       var nodeObj = nodes[node];
+	   nodeObj.id = node;
+	   chartJSON.nodes.push(nodeObj);
+     }
+    }
+	
+	for (var conn in conns) {
+     if (conns.hasOwnProperty(conn)) {
+       var connObj = {
+		   start: conns[conn].start,
+		   end: conns[conn].end
+	   };
+	   chartJSON.connections.push(connObj);
+     }
+    }
+	
+	return chartJSON;
 }
